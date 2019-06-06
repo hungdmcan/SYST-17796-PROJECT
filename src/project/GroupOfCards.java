7a8646b8f2ef0c142178403b40bf03dcb0e7f0a5
Mike Dao <hungdm@gmail.com>
2019-06-06 16:46:51 -0400

    Update BaseCode.vpp

--- src/project/GroupOfCards.java
+++ src/project/GroupOfCards.java
@@ -1,57 +1,59 @@
-/**
- * SYST 17796 Project Summer 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package project;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * A concrete class that represents any grouping of cards for a Game.
  * HINT, you might want to subclass this more than once.
  * The group of cards has a maximum size attribute which is flexible for reuse.
+ *
  * @author megha,2019
  */
-public class GroupOfCards 
+public class GroupOfCards
 {
-   
-    //The group of cards, stored in an ArrayList
-    private ArrayList <Card> cards;
-    private int size;//the size of the grouping
-    
-    public GroupOfCards(int givenSize)
-    {
-        size = givenSize;
-    }
-    
-    /**
-     * A method that will get the group of cards as an ArrayList
-     * @return the group of cards.
-     */
-    public ArrayList<Card> showCards()
-    {
-        return cards;
-    }
-    
-    public void shuffle()
-    {
-        Collections.shuffle(cards);
-    }
 
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
+   private int size;
+   /**
+    * The group of cards, stored in an ArrayList
+    */
+   private Collection<Card> cards;
 
-    /**
-     * @param givenSize the max size for the group of cards
-     */
-    public void setSize(int givenSize) {
-        size = givenSize;
-    }
-    
-}//end class
+   public int getSize ()
+   {
+      return this.size;
+   }
+
+   public void setSize (int size)
+   {
+      this.size = size;
+   }
+
+   /**
+    * the size of the grouping
+    *
+    * @param givenSize
+    */
+   public GroupOfCards (int givenSize)
+   {
+      // TODO - implement GroupOfCards.GroupOfCards
+      throw new UnsupportedOperationException();
+   }
+
+   /**
+    * A method that will get the group of cards as an ArrayList
+    *
+    * @return the group of cards.
+    */
+   public java.util.ArrayList<Card> showCards ()
+   {
+      // TODO - implement GroupOfCards.showCards
+      throw new UnsupportedOperationException();
+   }
+
+   public void shuffle ()
+   {
+      // TODO - implement GroupOfCards.shuffle
+      throw new UnsupportedOperationException();
+   }
+
+}
