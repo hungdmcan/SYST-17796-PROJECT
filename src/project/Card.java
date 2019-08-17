720c1bda5548199ca7eb632aae6ce9dbb8a784fc
Mike Dao <hungdm@gmail.com>
2019-08-16 20:07:25 -0400

    Update PasswordValidator Class

--- src/project/Card.java
+++ src/project/Card.java
@@ -1,21 +1,75 @@
 package project;
-
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code
- * should remember to add themselves as a modifier.
- *
- * @author megha, 2019
+ * @author Rich Smith at ZenOfProgramming.com
  */
-public abstract class Card
+public class Card
 {
-
-   /**
-    * Students should implement this method for their specific children classes
-    *
-    * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+   /*
+    * Creates a playing card.
     */
-   @Override
-   public abstract String toString ();
+   private int rank;//represents the rank of a card
+   private int suit;//represents the suit of a card
+   private int value;//represents the value of a card
+   private static String[] ranks = {"Joker", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+   private static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
+   /*
+    * Created with an integer that represents a spot in the String array ranks and the String array suits. This represents
+    * the rank and suit of an individual card.
+    */
+   Card (int suit, int values)
+   {
+      this.rank = values;
+      this.suit = suit;
+   }
+
+   /*
+    * Returns the string version of a card.
+    * what about image version of a card
+    */
+   public String toString ()
+   {
+      return ranks[rank] + " of " + suits[suit];
+   }
+
+   /*
+    * Returns the rank of a card.
+    */
+   public int getRank ()
+   {
+      return rank;
+   }
+
+   /*
+    * Returns the suit of a card.
+    */
+   public int getSuit ()
+   {
+      return suit;
+   }
+
+   /*
+    * Returns the value of a card. If a jack, queen, or king the value is ten. Aces are 11 for now.
+    */
+   public int getValue ()
+   {
+      if (rank > 10) {
+         value = 10;
+      }
+      else if (rank == 1) {
+         value = 11;
+      }
+      else {
+         value = rank;
+      }
+      return value;
+   }
+
+   /*
+    * Sets the value of a card.
+    */
+   public void setValue (int set)
+   {
+      value = set;
+   }
 }
